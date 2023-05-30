d3d7dd472ac99f8283f9eff7c7e87e5826d15257
rishabh11336 <rishabh11336@gmail.com>
2023-05-30 21:11:19 +0530

    feature of create new article is working

--- frontend/src/components/CreAte.vue
+++ frontend/src/components/CreAte.vue
@@ -1,10 +1,54 @@
 <template>
-  <h1>Create Page</h1>
+  <div class="container mt-4">
+    <form @submit.prevent="insertArticle">
+      <input type="text" class="form-control" placeholder="Please Enter your Title" v-model="title" /><br>
+      <textarea rows="10" class="form-control" placeholder="Please enter your body" v-model="body"></textarea>
+      <button class="btn btn-success mt-4">
+        Prublish Article
+      </button>
+    </form>
+    <div v-if="error" class="alert alert-warning alert-dismissible fade show mt-5" role="alert">
+      <strong>{{error}}</strong>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
 
+  data() {
+    return {
+    title:null,
+    body:null,
+    error:null
+  }
+  },
+
+
+  methods: {
+    insertArticle() {
+      if (!this.title || !this.body) {
+        this.error = "Please add all fields"
+      } else {
+        fetch('http://127.0.0.1:5000/add', {
+        method:"POST",
+        headers: {
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify({title:this.title, body:this.body}) 
+      })
+      .then(resp => resp.json())
+      .then(() => {
+        this.$router.push({
+          name: 'home'
+        })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+      }
+    }
+  }
 }
 </script>
 
